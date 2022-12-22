a889c7a5c48c3b7bf4bad893c5307d4fd525ec11
Aubrey Corsetti <aubreylcorsetti@icloud.com>
2022-12-21 16:28:16 -0800

    completed lab 3, added test_madlib, added requirements, updated README

---
+++ README.md
@@ -0,0 +1,12 @@
+LAB - Class 03
+Project: Madlib
+Author: Aubrey Corsetti
+
+How to run app: python madlib_cli/madlib.py
+
+Tests:
+I click the play button to run my code located in the middle of the toolbar at the top of the page.
+
+Any tests of note? I did tests along every step to ensure it was correct and there were no typos.
+
+Describe any tests that you did not complete? I completed the requirements
---
+++ madlib_cli/madlib.py
@@ -0,0 +1,39 @@
+from textwrap import dedent
+import re
+
+
+def read_template(file):
+    try:
+        with open(file) as f:
+            return f.read()
+    except FileNotFoundError as fnf_error:
+        raise fnf_error
+
+
+def parse(string):
+    words = tuple(re.findall(r"{([^{}]*)}", string))
+    for x in words:
+        string = string.replace(x, "")
+        return string, words
+
+
+def merge(stripped, inputs):
+    return stripped.format(*inputs)
+
+
+def welcome():
+    print(dedent("""
+    Heyo! Welcome to the MAD world of madlib!
+    To play: please follow the prompts!"""))
+
+
+welcome()
+# def story = ("It was a {Adjective} and {Adjective} {Noun}.")
+
+
+adjective = input("Enter an adjective: ")
+adjective2 = input("Enter an adjective: ")
+noun = input("Enter a noun: ")
+
+story = ("It was a " + adjective + " and " + adjective2 + " " + noun)
+print(story)
